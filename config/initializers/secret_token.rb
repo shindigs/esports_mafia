9b4e5ce27fa9cac4daf90b8b829779e443851189
Timothy Young <shindigs1@gmail.com>
2013-10-07 02:41:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EsportsMafia::Application.config.secret_key_base = 'eeb1d06a317d8d55b2b326a66a1bb8389aa5c28c1cb811af4d95a3257a8e61eb4251aba616aed539cf4d898dfbfa4e4320a5c4cf0c738f236e0d5d501d1987c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EsportsMafia::Application.config.session_store :cookie_store, key: '_esports_mafia_session'
